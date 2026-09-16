65730a8f734e27ac1a6b67d7956609000470d499
4nasirsaifi-art <4nasirsaifi@gmail.com>
2026-09-16 14:42:23 +0530

    change in py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "GitHub Actions -> Docker Hub -> Azure VM Deployment Successful!"
+    return "my auto deployment is working Successful!"
 
 app.run(host='0.0.0.0', port=5000)
